42167769cf3c2450acd5247e5753c6140ad78203
Muzib Ahamed <muzib.a@gmail.com>
2020-11-18 19:25:55 -0600

    Initial Commit

---
+++ HelloSwiftUI2/HelloSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUI2App.swift
+//  HelloSwiftUI2
+//
+//  Created by Muzib Ahamed Shaik on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
